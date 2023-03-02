31a52e33b8889c512582da85b32a1f71fae4479d
rogdevil <sep2001aryan@yahoo.com>
2023-03-03 02:28:27 +0530

    script ready

---
+++ index.js
@@ -0,0 +1,52 @@
+import puppeteer from 'puppeteer';
+
+(async () => {
+	const browser = await puppeteer.launch({
+		headless: false,
+	});
+	const page = await browser.newPage();
+	const context = await browser.defaultBrowserContext()
+	  context.overridePermissions('https://webmobinfo.kredily.com', ['notifications', 'camera', 'geolocation'])
+	// context.overridePermissions('https://getnada003.kredily.com/', ['notifications', 'camera', 'geolocation'])
+	
+	await page.goto('https://app.kredily.com/login/');
+	// other actions...
+	await page.waitForSelector('#signInFormEmailAddress')
+	await page.type('#signInFormEmailAddress', '');
+	await page.type('#signInFormPassword', '');
+	await page.click('#signinSubmitBtn')
+	await page.waitForNavigation();
+	await page.setRequestInterception(true)
+	page.on('request', (request) => {
+		if (request.url().includes('https://getnada003.kredily.com')) {
+			request.continue()
+		}
+	})
+
+	page.on('response', async (response) => {
+		if(response.url().includes('clockOut/')){
+			if(response.status() >= 200 && response.status() < 300){
+				console.log("Clockout successful")
+				await page.setRequestInterception(false)
+				await browser.close();
+			} else {
+				console.log("Clockout was not success please try again")
+				await page.setRequestInterception(false)
+				await browser.close();
+			}
+		}
+		
+		if(response.url().includes('clockIn/')){
+			if(response.status() >= 200 && response.status() < 300){
+				console.log("ClockIn successful")
+				await page.setRequestInterception(false)
+				await browser.close();
+			} else {
+				console.log("ClockIn was not success please try again")
+				await page.setRequestInterception(false)
+				await browser.close();
+			}
+		}
+	})
+	await page.evaluate("clockInAction(document.querySelector('.clockInBtn'))")
+})();
